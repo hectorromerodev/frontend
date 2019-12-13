edecf8b56622fc5ddb46a0e07895c6d10fe62159
Hector A. Romero Morales <harm0294@GMAIL.COM>
2019-12-12 17:14:34 -0800

    login

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"]
 })
 export class SignupComponent implements OnInit {
+  user = {};
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  signUp() {
+    this.authService.signUp(this.user).subscribe(
+      res => {
+        localStorage.setItem("token", res.token);
+        this.router.navigate(["/profile"]);
+      },
+      err => console.log(err)
+    );
   }
-
 }
