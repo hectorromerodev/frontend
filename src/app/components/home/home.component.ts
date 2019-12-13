5fafe9bae6b26d3e65ee49acb1a250d3a1ad1215
Hector A. Romero Morales <harm0294@GMAIL.COM>
2019-12-12 17:15:04 -0800

    home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  users = [];
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
+    this.userService.getProfile().subscribe(
+      res => {
+        this.users = res;
+      },
+      err => console.log(err)
+    );
   }
-
 }
